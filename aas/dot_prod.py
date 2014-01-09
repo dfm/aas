a1b79159f77397fca7135a2e3bb6f93b47eaa627
Dmitry <dvmalyshev@gmail.com>
2014-01-09 14:01:25 -0500

    test of the dot product

--- aas/dot_prod.py
+++ aas/dot_prod.py
@@ -1,7 +1,7 @@
 # scalar product in the space of texts
 
-import nltk
-import numpy
+#import nltk
+import numpy as np
 
 def norm2(g, a):
     """
@@ -25,6 +25,8 @@ def product(g, a, b):
     calculates the product of a and b with metric g
     INPUT:
         g - dictionary, metric
+            for example, {'word': 1 / log(1 + n['word']), ...}
+            where n['word'] is the number of times 'word' occurs in texts
         a - dictionary, user vector
         b - dictionary, test text vector
         
@@ -45,3 +47,24 @@ def product(g, a, b):
     return res
 
 
+if __name__ == '__main__':
+    words = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    nw = len(words)
+    n = np.arange(1., nw + 1., 1.)
+    a = {}
+    b = {}
+    g = {}
+    for i in range(nw):
+        g[words[i]] = 1. / np.log(1. * i + 2.)
+        
+    for i in range(4):
+        a[words[i]] = i * 2. + 1.
+        b[words[nw - i - 1]] = i * 2. + 1.
+
+    prod = product(g, a, b)
+
+    print 'metric:', g
+    print 'user vector:', a
+    print 'text vector:', b
+    print 'scalar product:', prod
+    
