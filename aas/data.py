d50bdfe944b7c87425196d826e98d82c4a39c1f4
Dan F-M <danfm@nyu.edu>
2014-01-09 13:21:37 -0500

    adding python helper for dataset

--- aas/data.py
+++ aas/data.py
@@ -4,9 +4,10 @@
 from __future__ import (division, print_function, absolute_import,
                         unicode_literals)
 
-__all__ = []
+__all__ = ["Dataset"]
 
 import json
+from nltk import sent_tokenize, word_tokenize
 
 
 class Dataset(object):
@@ -14,5 +15,24 @@ class Dataset(object):
     def __init__(self, path):
         with open(path, "r") as f:
             sessions = json.load(f)
-            for key, session in sessions.iteritems():
-                pass
+
+        self.data = {}
+        for key, session in sessions.iteritems():
+            for pid, pres in session["presentations"].iteritems():
+                self.data[pres["id"]] = self.parse(pres["title"]
+                                                   + " " + pres["abstract"])
+
+    def parse(self, text):
+        return [w for s in map(word_tokenize, sent_tokenize(text)) for w in s]
+
+    def __iter__(self):
+        for id_, doc in self.data.iteritems():
+            yield doc
+
+
+if __name__ == "__main__":
+    print("Loading")
+    dataset = Dataset("data/abstracts.json")
+    print("Loaded")
+    for doc in dataset:
+        print(doc)
